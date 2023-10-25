3aa6dd3f1805c1d03e9c146a2363ffc651ed2b31
“Xaxxoo” <bnahmad83@gmail.com>
2023-10-25 21:29:18 +0100

    Error handlers

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 require("dotenv").config();
+import { ErrorMiddleware } from "./middleware/error";
 
 //BodyParser
 app.use(express.json({ limit: "50mb" }));
@@ -31,3 +32,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+
+app.use(ErrorMiddleware)
